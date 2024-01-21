d6895b1956c836bbf7852c178de874582941da58
Amanda Tinoco <amanda_tinoco00@hotmail.com>
2024-01-21 11:07:24 -0300

    style: unused imports

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { Stack, Box, Heading, Text, Container  } from "@chakra-ui/react";
+import { Container  } from "@chakra-ui/react";
 
 
 function Footer() {
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,39 +1,33 @@
-import React, { useEffect, useState } from "react";
 import Card from "./Card"
 import Banner from "./Banner"
 import Footer from "./Footer"
-
-
-const SupplierContext = React.createContext({
-supplier: [], fetchSupplier: () => {}
-})
 
 
 export default function Home() {
 
     return (
         <div>
-            <Card 
+            <Card
                 className='section'
                 title='Sobre Nós'
-                img="/img/raio.jpg" 
+                img="/img/raio.jpg"
                 description='A Clarke dá suporte total à sua empresa para que ela consiga comprar eletricidade diretamente de geradores e comercializadores no Mercado Livre de Energia.'
             />
             <Banner />
-            <Card 
+            <Card
                 className='section'
                 title='Nossos diferenciais'
-                img="/img/energia_limpa.jpg" 
+                img="/img/energia_limpa.jpg"
                 description='Atendimento Consultivo, Melhores preços, Plataforma tecnológica'
             />
-            <Card 
+            <Card
                 className='section'
                 title='No Mercado Livre de Energia, a economia é de até 40% na conta'
-                img="/img/energia_limpa.jpg" 
+                img="/img/energia_limpa.jpg"
                 description='Quem participa do Mercado Livre de Energia ou Ambiente de Contratação Livre (ACL) pode negociar todas as condições comerciais das contratações, como fornecedor, quantidade, preço, período de suprimento e forma de pagamento.'
             />
-            <Footer/>
+            <Footer />
         </div>
-        
+
     );
-  };
+};
